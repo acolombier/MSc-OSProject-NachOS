852134eafcf02e6e15fdd6ce4d58c655ffc8f485
Sayak Samaddar <samaddsa@F202-01.e-im2ag.ujf-grenoble.fr>
2017-12-13 16:26:53 +0100

    putstring syscall first try

---
+++ code/putstring.c
@@ -0,0 +1,31 @@
+#include "syscall.h"
+
+int main(){
+	int len=0;
+	char input[20]="Hello World";
+	while(input[i]!='\0')
+	 len++;
+	for(int i=0;i<len;i++)
+	 putchar(input[i]);
+	while(getchar(input[i]==false)
+			semwait();
+	}
+	Halt();
+
+}
+
+void getchar(){
+
+void print(char c){
+
+	PutChar(c);
+
+}
+int main(){
+	char ch;
+	ch=GetChar();
+	PutChar(ch);
+	Halt();
+
+}
+}
