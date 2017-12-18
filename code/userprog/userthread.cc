dd8b515fdb2033794b92037dd5a3f0807a549ece
sayak <sayaksam@gmail.com>
2017-12-18 17:00:37 +0100

    userthreadcreate first try

--- code/userprog/userthread.cc
+++ code/userprog/userthread.cc
@@ -1,5 +1,6 @@
 #include "system.h"
 #include "userthreads.h"
+#define PAGESPERTHREAD 10
 
 typedef struct bundle {
     int function;
@@ -25,4 +26,46 @@ int do_UserThreadCreate(int f, int arg) {
     t->Fork(StartUserThread, bundle);
 
     return 0;
+}
+
+static void StartUserThread (int f){
+
+  DEBUG('t', "Inside start user thread\n");
+  	int Fnaddress = (bundle* f->function);
+  	int arg_list = (bundle*f->arg);
+//TODO Remove after testing the control of program
+  	int tid;
+    tid=  currentThread->getThreadID();
+    fprintf(stderr, "SC %d %d\n", Fnaddress,arg_list );
+    fprintf(stderr, "Thread Execution  %d\n",tid );
+    //upto this part
+    currentThread->space->RestoreState();
+    currentThread->space->InitRegisters();
+
+  	machine->WriteRegister(4, arg_list);
+  	machine->WriteRegister (PCReg,Fnaddress);
+  	machine->WriteRegister (NextPCReg, Fnaddress + 4);
+  	//int temp_stack_addr = currentThread->space->numPages ;
+  	//temp_stack_addr = machine->ReadRegister(StackReg);
+  	int pos;
+
+  	pos = currentThread->space->GetStack();
+  	if (pos == -1)
+  	{
+  		fprintf(stderr, "Insufficient Space for New Thread Creation \n" );
+  		do_UserThreadExit();
+  	}
+  	currentThread->bitmapID= pos;
+
+  	//fprintf(stderr, "%d\n", temp);
+  	machine->WriteRegister (StackReg,  PAGESPERTHREAD *(pos+1) * PageSize);
+  	//machine->WriteRegister (StackReg, currentThread->space->numPages*PageSize -16 - 3 * tid * PageSize);
+
+  	// call this function to run the system
+  	machine->Run();
+
+
+
+
+  }
 }
