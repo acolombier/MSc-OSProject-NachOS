d5642ed49f60935c40624887e277645328e92cab
ledentp <philippe.ledent.1@gmail.com>
2018-01-11 10:42:19 +0100

    reindented file

--- code/test/makethreads.c
+++ code/test/makethreads.c
@@ -11,6 +11,7 @@ void* func(void *arg) {
     for (i = 0; i < n; i++) {
         PutString("func ");
         PutInt(i);
+	PutChar('\n');
     }
 
     //~ UserThreadExit();
@@ -20,12 +21,12 @@ void* func(void *arg) {
 int main() {
 
     PutString("In main\n");
-	PutString("Creating thread #");
-	PutInt(UserThreadCreate(func, (void *) 5));
-	PutString("\nCreating thread #");
-	PutInt(UserThreadCreate(func, (void *) 10));
-	PutString("\nCreating thread #");
-	PutInt(UserThreadCreate(func, (void *) 15));
-	PutString("\nHalting...");
+    PutString("Creating thread #");
+    PutInt(UserThreadCreate(func, (void *) 5));
+    PutString("\nCreating thread #");
+    PutInt(UserThreadCreate(func, (void *) 10));
+    PutString("\nCreating thread #");
+    PutInt(UserThreadCreate(func, (void *) 15));
+    PutString("\nHalting...");
     Halt();
 }
