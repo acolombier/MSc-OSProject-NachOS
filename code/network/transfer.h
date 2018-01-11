2fba9dfd7367c0f2df83583ac4ccf22b22115cd8
Benjamin Collet <benjamin.collet@protonmail.ch>
2018-01-11 10:40:26 +0100

    finished the network header def

--- code/network/transfer.h
+++ code/network/transfer.h
@@ -8,47 +8,48 @@
 #define TEMPO 1000
 #define MAXREEMISSIONS 5
 
-#define MaxPacketSize 	(MaxWireSize - sizeof(struct PacketHeader) - sizeof(struct ConnectionHeader))	
+#define MaxPacketSize (MaxWireSize - sizeof(struct PacketHeader) - sizeof(struct TransferHeader))
 
-// The following class defines part of the message header.  
-// This is prepended to the message by the RTFM, before the message 
+// The following class defines part of the message header.
+// This is prepended to the message by the RTFM, before the message
 // is sent to the Network.
+
+enum {
+    ACK = 1 << 2,
+    SYN = 1 << 1,
+    FIN = 1 << 0
+};
 
 class TransferHeader {
   public:
-    MailBoxAddress to;		// Destination mail box
-    MailBoxAddress from;	// Mail box to reply to
-    unsigned length;		// Bytes of message data (excluding the 
-				// transfer header)
-	int ackReceivedFrom;	// When using Send, we want ack from the address we sent message to
-	int ackSentTo;			// When using Receive, we want to send ack to the address we received message from
+    MailHeader mailHeader;
+    char flags;
+    int seqNumber;
 };
 
-class RTFM {
-	public:
-		RTFM(to, mailbox);
-		~RTFM();
+class Connection {
+  public:
+    Connection(NetworkAddress to, MailBoxAddress mailbox);
+    ~Connection();
 
-		int Send(char *data, FixedHeader header); // send a message reliably with ack
-		int Receive(char *data);	// receive a message reliably with ack, store it in data
-		int SendFile(int fd, int fileSize);	// send a file from a client machine to remote machine
-		int ReceiveFile(int fd, int fileSize);	// receive a file from a remote machine to a client machine
+    int Send(char *data);  // send a message reliably with ack
+    int Receive(char *data);  // receive a message reliably with ack, store it in data
+    int SendFile(int fd, int fileSize);  // send a file from a client machine to remote machine
+    int ReceiveFile(int fd, int fileSize);  // receive a file from a remote machine to a client machine
 
-	private:
-		int SendFixedSize(char *data);	// send a message of size <= MaxPacketSize
-		int sendVariableSize(char *data);	//	send a message of size > MaxPacketSize
-		int ReceiveFixedSize(char *data);	// receive a message of size <= MaxPacketSize
-		int ReceiveVariableSize(char *data);	//	receive a message of size > MaxPacketSize
-		
-	
+  private:
+    int SendFixedSize(char *data);  // send a message of size == MaxPacketSize
+    int ReceiveFixedSize(char *data);  // receive a message of size == MaxPacketSize
 };
+
 
 #endif  /* TRANSFER_H */
 
+
 /* usage example:
 
-RTFM conn = new RTFM(42, 1);
+Connection conn = new Connection(42, 1);
 conn->Send(*data);
 char *data = conn->Recieve();
-delete RTFM();
+delete conn;
 */
